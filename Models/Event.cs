0d556cf5b7141acd12b2ccd3b156bb2abf6c5ab7
Gayatri Aryal <aryal.gayatri@hotmail.com>
2025-06-15 01:13:37 -0400

    Initial Commit

---
+++ Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Models;
+
+public class Event
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+}
